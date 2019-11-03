aee2f3f81ef347cb55b23b2c333f8b2e1acaaf11
Raj Singh <singhrab@gmail.com>
2019-11-03 14:50:03 +0000

    removing client

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ appServer.use(express.static(path.join(__dirname, 'dist/opstinuum')));
 appServer.use(bodyParser.urlencoded({extended: true}));
 appServer.use(cors())
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/opstinuum/index.html'))
+appServer.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/opstinuum/index.html'))
 })
 
 appServer.use(bodyParser.json());
