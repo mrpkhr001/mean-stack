4aae57f687fdc66089f96090f045fa800041f4ea
Raj Singh <singhrab@gmail.com>
2019-11-03 11:20:17 +0000

    removing client

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+var https = require('https');
+var fs = require('fs');
+var cors = require('cors')
+
+const api = require('./server/routes/api');
+const serverPort = process.env.PORT || 8443;
+
+const appServer = express();
+
+appServer.use(express.static(path.join(__dirname, 'dist')));
+appServer.use(bodyParser.urlencoded({extended: true}));
+appServer.use(cors())
+
+appServer.use(bodyParser.json());
+appServer.use('/api', api);
+
+var hskey = fs.readFileSync('serverPrivateKey.pem');
+var hscert = fs.readFileSync('server.crt')
+
+var options = {
+  key: hskey,
+  cert: hscert
+};
+
+var server = https.createServer(options, appServer);
+
+server.listen(serverPort, function () {
+  console.log('Server Node Express server is up on 8443');
+});
